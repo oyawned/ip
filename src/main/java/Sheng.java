e0a5c5c609ac6f6d680d942f92611f6af3260334
oyawned <e1025501@u.nus.edu>
2026-01-25 17:23:33 +0800

    Level-2. Add, List

--- src/main/java/Sheng.java
+++ src/main/java/Sheng.java
@@ -4,6 +4,8 @@ public class Sheng {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         
         System.out.println(line);
         System.out.println(" Hello! I'm Sheng!");
@@ -16,8 +18,14 @@ public class Sheng {
             System.out.println(line);
             if (input.equals("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(" " + input);
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(" added: " + input);
             }
             System.out.println(line);
         } 
